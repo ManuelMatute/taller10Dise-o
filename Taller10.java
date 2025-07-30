5dfa182a3178ebc006ca2e1d3feecbe204af927d
Moreno0Josseph <150060469+Moreno0Josseph@users.noreply.github.com>
2025-07-30 15:20:54 -0500

    Add files via upload

---
+++ Taller10.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package stack.taller10;
+
+/**
+ *
+ * @author CltControl
+ */
+public class Taller10 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
